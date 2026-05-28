6239c284d97d0f028a436487d44ec2d23b5625d1
Neeraj Bhatia <57321215+seneeraj@users.noreply.github.com>
2026-05-28 20:06:08 +0530

    Update insert_document.py

--- database/insert_document.py
+++ database/insert_document.py
@@ -1,9 +1,6 @@
-from sqlalchemy import insert
-
-from datetime import datetime
-
-from database.db_manager import engine
-from database.models import documents
+from database.supabase_client import (
+    supabase
+)
 
 # =====================================================
 # INSERT DOCUMENT
@@ -14,43 +11,39 @@ def insert_document(
     document_name,
     document_category,
     encrypted_path,
-    ocr_text="",
-    tags="",
-    notes="",
-    expiry_date=None
+    ocr_text,
+    tags,
+    notes,
+    expiry_date
 ):
 
-    try:
+    data = {
 
-        query = insert(documents).values(
+        "document_name": document_name,
 
-            document_name=document_name,
+        "document_category": document_category,
 
-            document_category=document_category,
+        "encrypted_path": encrypted_path,
 
-            encrypted_path=encrypted_path,
+        "ocr_text": ocr_text,
 
-            ocr_text=ocr_text,
+        "tags": tags,
 
-            upload_date=datetime.now(),
+        "notes": notes,
 
-            tags=tags,
-
-            notes=notes,
-
-            expiry_date=expiry_date
+        "expiry_date": (
+            str(expiry_date)
+            if expiry_date
+            else None
         )
+    }
 
-        with engine.begin() as conn:
+    response = (
 
-            conn.execute(query)
+        supabase
+        .table("documents")
+        .insert(data)
+        .execute()
+    )
 
-        return True
-
-    except Exception as e:
-
-        print(
-            f"Insert document error: {e}"
-        )
-
-        return False
+    return response
